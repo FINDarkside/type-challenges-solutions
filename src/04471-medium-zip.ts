160f9f46303ba9176bfad21e54e41cfd0e22feba
Jesse Niininen <jesse.niininen@inderes.fi>
2023-09-26 23:27:35 +0300

    Solve 4471

---
+++ src/04471-medium-zip.ts
@@ -0,0 +1,17 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ["1", "2"]>, [[1, "1"], [2, "2"]]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>
+];
+
+// ============= Your Code Here =============
+type Zip<T, U> = T extends [infer Head1, ...infer Rest1]
+  ? U extends [infer Head2, ...infer Rest2]
+    ? [[Head1, Head2], ...Zip<Rest1, Rest2>]
+    : []
+  : [];
